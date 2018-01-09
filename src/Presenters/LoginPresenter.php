eff9f5759ff78a0cded175313808497228d5a7a9
Frantisek Vesely <vesely@grandmedia.z>
2018-01-09 16:25:05 +0100

    Add Login layout and trait

---
+++ src/Presenters/LoginPresenter.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace GrandMedia\AdminLTE\Presenters;
+
+trait LoginPresenter
+{
+
+	/** @var string */
+	protected $baseLayout = __DIR__ . '/templates/@login.layout.latte';
+
+}
